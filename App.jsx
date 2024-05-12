f5ff507546756c68d3b31904e989ce9794a26e13
Alejandro Armas <lxarmas@gmail.com>
2024-05-12 10:42:06 +0200

    add_data

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./components/Header";
 import Meme from './components/Meme';
+import memesData from './memesData';
 
 function App() {
 
--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+
 
 const Meme = () => {
+    function handleClick() {
+        console.log('I was clicked')
+    }
   return (
       <main>
-          <form className='form'>
+          <div className='form'>
               <input
                   type="text"
                   placeholder='Top text'
-                  className='form-input'
-              />
+                  className='form--input'
+                  />
+            
               <input
                   type="text"
                   placeholder='bottom-text'
-                  className='form-input'
-              />
+                  className='form--input'
+                      />
+                  
               <button
+                  onClick={handleClick}
                className='form--button'   
               >Get a new meme Image</button>
           </form>
