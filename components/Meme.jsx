25f4dbf7ead420c8946f0eb566d2a1e10b879ef7
Alejandro Armas <lxarmas@gmail.com>
2024-05-29 09:31:41 +0200

    finish

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -10,10 +10,14 @@ export default function Meme() {
     })
     const [allMemes, setAllMemes] = React.useState([])
     
-    React.useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(data => setAllMemes(data.data.memes))
+    React.useEffect( () => {
+        
+        async function getMemes() {
+            const res = await fetch( "http://api.imgflip.com/get_memes" )
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
     }, [])
     
     
